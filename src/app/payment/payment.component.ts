f875cafeb37b05c282aa8ad8540d07b155b271cb
Sabrina Dias <sabrina.dias@DFT-SP-WKN585.dafiti.local>
2020-06-15 19:41:49 -0300

    add module payment

---
+++ src/app/payment/payment.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-payment',
+  templateUrl: './payment.component.html',
+  styleUrls: ['./payment.component.scss']
+})
+export class PaymentComponent implements OnInit {
+
+  constructor() { }
+  @Input() userChoice: any;
+  ngOnInit() {
+  }
+
+}
