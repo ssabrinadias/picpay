a68e97db6cb841ae0230b7cbf8969318667e0362
Sabrina Dias <sabrina.dias@DFT-SP-WKN585.dafiti.local>
2020-06-16 17:50:44 -0300

    change  environments url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,10 @@
 export const environment = {
   production: false,
   API: 'https://www.mocky.io/v2/',
-  WALLET_URL: 'https://run.mocky.io/v3/35b37f33-578d-4609-948a-d7fcb4a815d4'
+  WALLET_URL: 'https://demo9804355.mockable.io/wallet', // 'https://run.mocky.io/v3/35b37f33-578d-4609-948a-d7fcb4a815d4',
+  USERS_URL: 'https://demo9804355.mockable.io/users', // 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce',
+  TRANSACTION_URL: 'https://demo9804355.mockable.io/transaction', // 'https://www.mocky.io/v2/5d542ec72f000048248614b3',
+  WALLET_TOKEN_URL: 'https://demo9804355.mockable.io/card/token/',
 };
 
 /*
